7f8fd84e5d38146d823f879383617a195b2d2720
David <david.ervin@gmail.com>
2018-10-03 22:18:41 -0400

    Integrated the initial data loader as a Bean, made it run on startup

--- src/main/java/us/ervin/providerdir/providerdirectory/ProviderDirectoryApplication.java
+++ src/main/java/us/ervin/providerdir/providerdirectory/ProviderDirectoryApplication.java
@@ -1,12 +1,23 @@
 package us.ervin.providerdir.providerdirectory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("us.ervin.providerdir.providerdirectory")
 public class ProviderDirectoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderDirectoryApplication.class, args);
+	}
+	
+	
+	// CommandLineRunner gets invoked as soon as the app starts
+	@Bean
+	public CommandLineRunner loadIntialData(InitialDataLoader loader) {
+		return args -> loader.loadData();
 	}
 }
