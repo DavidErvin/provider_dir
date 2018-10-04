0b9c1f59f5e3aa848c9849b6354448e11776c3d2
David <david.ervin@gmail.com>
2018-10-03 21:35:26 -0400

    Spring Boot (web, rest, H2) starter

---
+++ src/main/java/us/ervin/providerdir/providerdirectory/ProviderDirectoryApplication.java
@@ -0,0 +1,12 @@
+package us.ervin.providerdir.providerdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderDirectoryApplication.class, args);
+	}
+}
---
+++ src/test/java/us/ervin/providerdir/providerdirectory/ProviderDirectoryApplicationTests.java
@@ -0,0 +1,16 @@
+package us.ervin.providerdir.providerdirectory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProviderDirectoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
