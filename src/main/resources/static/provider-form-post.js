ec4fb7717f07e87428e7b8bbe0ae2044013b44d3
David <david.ervin@gmail.com>
2018-10-11 20:56:51 -0400

    Ripping JS out to its own files, stubbed in provider table rendering

---
+++ src/main/resources/static/provider-form-post.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	// handle provider submissions
+	$('form').submit(function(event) {
+		// build the JSON structure we'll send over
+		var formData = {
+			'first_name' : $('input[name=first_name]').val(),
+			'last_name' : $('input[name=last_name]').val(),
+			'email_address' : $('input[name=email_address]').val(),
+			'specialty' : $('input[name=specialty]').val(),
+			'practice_name' : $('input[name=practice_name]').val()
+		};
+
+		// POST the form contents to the /provider REST endpoint
+		$.ajax({
+			type : 'POST',
+			url : 'provider',
+			data : JSON.stringify(formData),
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+			encode : false
+		}).done(function(data) {
+			// log data to the console so we can see
+			console.log(data);
+		});
+
+		// stop the form from submitting the normal way and refreshing the page
+		event.preventDefault();
+
+		// clear out the form with plain old JS
+		document.getElementById("create_provider").reset();
+	});
+});
---
+++ src/main/resources/static/provider-table-load.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	// get the provider table body
+	var providerTable = $("#provider-container");
+	
+	// get providers from the REST endpoint
+	$.ajax({
+		type: 'GET',
+		url: 'provider',
+		dataType: 'json'
+	}).done(function(json) {
+		// not a string, but real live JSON!
+		$.each(json, function(i, item) {
+			// now I've got an individual provider record
+			var HTML = 
+				'<div class="container-fluid">' +
+					'<div class="row">' +
+						'<input class="col-1" type="checkbox" id="provider_id_check_x"></input>' +
+						'<div class="col-6" align="left">' +
+						'<h5>Last Name, First Name</h5>' +
+					'</div>' +
+					'<div class="col-5" align="right">' +
+						'<p><b>Specialty</b></p>' +
+					'</div>' +
+				'</div>' +
+				'<div class="row">' +
+					'<div class="col-1"></div>' +
+						'<div class="col-5">' +
+							'<p>Email address</p>' +
+						'</div>' +
+						'<div class="col-6" align="right">' +
+							'<p>Practice name</p>' +
+						'</div>' +
+					'</div>' +
+				'</div>';
+			
+			var providerContent = $(HTML);
+			providerContent.appendTo(providerTable);
+		});
+	});
+});
